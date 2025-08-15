395e61e627831a5c48e298c5fd4131660578ba04
e-mustafa <eng.mustafax@gmail.com>
2025-08-15 20:11:31 +0300

    fix noreferrer link

--- src/Components/SocialContact.jsx
+++ src/Components/SocialContact.jsx
@@ -1,24 +1,36 @@
-import { Box, Stack, Tooltip } from "@mui/material";
+import { Box, Stack, Tooltip } from '@mui/material';
 
-export default function SocialContact({data = []}) {
-   return (
-      <Stack
-         display='flex'
-         flexDirection='row'
-         alignItems='center'
-         gap={2}
-         justifyContent={{ xs: 'center', lg: 'left' }}
-         className='animate__animated animate__zoomIn'
-      >
-         {data && data.map((item) => (
-            <Tooltip key={item.id}  title={item.title} arrow>
-               <a href={item.link} target='_blank' rel='noopener' >
-                  <Box className='social-icons' bgcolor='action.disabledBackground' color='text.disabled' sx={{ '&:hover': { color: 'text.white' } }} >
-                     <item.icon/>
-                  </Box>
-               </a>
-            </Tooltip>
-         ))}
-      </Stack>
-   )
+export default function SocialContact({ data = [] }) {
+	const handleClick = (e, link) => {
+		e.preventDefault();
+		window.open(link, '_blank', 'noopener');
+		// if (item.link.includes('linkedin.com')) {}
+	};
+
+	return (
+		<Stack
+			display='flex'
+			flexDirection='row'
+			alignItems='center'
+			gap={2}
+			justifyContent={{ xs: 'center', lg: 'left' }}
+			className='animate__animated animate__zoomIn'
+		>
+			{data &&
+				data.map((item) => (
+					<Tooltip key={item.id} title={item.title} arrow>
+						<a onClick={(e) => handleClick(e, item.link)} href={item.link} target='_blank' rel='noopener noreferrer'>
+							<Box
+								className='social-icons'
+								bgcolor='action.disabledBackground'
+								color='text.disabled'
+								sx={{ '&:hover': { color: 'text.white' } }}
+							>
+								<item.icon />
+							</Box>
+						</a>
+					</Tooltip>
+				))}
+		</Stack>
+	);
 }
